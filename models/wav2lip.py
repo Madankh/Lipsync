fd013a0d049081dfef8e2b9053e5cf8717579ba3
Madankh <khadkasuman0000@gmail.com>
2024-05-10 21:00:33 +0545

    improve Wav2lip

--- models/wav2lip.py
+++ models/wav2lip.py
@@ -55,7 +55,60 @@ class Wav2Lip(nn.Module):
             Conv2d(512, 512, kernel_size=1, stride=1, padding=0),)
 
         self.face_decoder_blocks = nn.ModuleList([
-            nn.Sequential(
-                
-            )
-        ]) 
+            nn.Sequential(Conv2d(512, 512, kernel_size=1, stride=1, padding=0),),
+
+            nn.Sequential(Conv2dTranspose(1024, 512, kernel_size=3, stride=1, padding=0), # 3,3
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),),
+
+            nn.Sequential(Conv2dTranspose(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
+            Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),), # 6, 6
+
+            nn.Sequential(Conv2dTranspose(768, 384, kernel_size=3, stride=2, padding=1, output_padding=1),
+            Conv2d(384, 384, kernel_size=3, stride=1, padding=1, residual=True),
+            Conv2d(384, 384, kernel_size=3, stride=1, padding=1, residual=True),), # 12, 12
+
+            nn.Sequential(Conv2dTranspose(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
+            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
+            Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),), # 24, 24
+
+            nn.Sequential(Conv2dTranspose(320, 128, kernel_size=3, stride=2, padding=1, output_padding=1), 
+            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
+            Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),), # 48, 48
+
+            nn.Sequential(Conv2dTranspose(160, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
+            Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),),]) # 96,96
+
+        self.output_block = nn.Sequential(Conv2d(80, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
+            nn.Sigmoid())
+        
+        def forward(self,audio_sequences, face_sequences):
+            # audio_sequences = (B, T, 1, 80, 16)
+
+            B = audio_sequences.size(0)
+            input_dim_size = len(face_sequences.size())
+            if input_dim_size > 4:
+                audio_sequences = torch.cat([audio_sequences[:,i] for i in range(audio_sequences.size(1))], dim=0)
+                face_sequences = torch.cat([face_sequences[:,i] for i in range(face_sequences.size(1))], dim=0)
+            audio_embedding = self.audio_encoder(audio_sequences) # B , 512, 1, 1
+            feats = []
+            x = face_sequences
+            for f in self.face_encoder_blocks:
+                x = f(x)
+                try:
+                    x = torch.cat((x, feats[-1]), dim=1)
+                except Exception as e:
+                    print(x.size())
+                    print(feats[-1].size())
+                    raise e
+
+                feats.pop()
+            x = self.output_block(x)
+
+            if input_dim_size > 4:
+                x = torch.split(x, B, dim=0)
+                outputs = torch.stack(x, dim=2)
+            else:
+                output = x
